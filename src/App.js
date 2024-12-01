915aa66c8fb0e90a769f9bd6dfa59a66da3fd318
evan-poster <evan.poster@gmail.com>
2024-12-01 18:12:38 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+	const message = "Hello from Group";
+	return (
+		<div style={{textAlign: 'center', marginTop: '20%'}}>
+			<h1>{message}</h1>
+		</div>
+	);
+}
+
+export default App;
